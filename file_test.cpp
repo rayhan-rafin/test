96ad5df790bc153f849a0307adf47dd6cad409e5
rayhan-rafin <rayhan.rafin2@gmail.com>
2022-12-29 18:00:42 +0600

    Update file_test.cpp

--- file_test.cpp
+++ file_test.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-    ofstream file("C:\rayhan\loc_test2\test3.txt");
+    ofstream file("C:/rayhan/loc_test2/test3.txt");
     file<<"This is a location test 3"<<endl;
     file.close();
     return 0;
